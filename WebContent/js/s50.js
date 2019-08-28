1bf417a6935bcbf3444e6b49ce3006efdf2f410c
Administrator <Administrator@192.168.12.53>
2019-08-28 18:02:25 +0200

    fine giornata 28/08/19 jquery bootstrap

--- WebContent/js/s50.js
+++ WebContent/js/s50.js
@@ -3,16 +3,41 @@
  * 
  * Slide 50
  */
+
 $(function() {
-    $('#colorOn').click(function() {
-        $('#msg1').addClass('red');
-    });
+	$('#colorOn').click(function() {
+		$('#msg1').addClass('red');
+	});
 
-    $('#colorOff').click(function() {
-        $('#msg1').removeClass('red');
-    });
+	$('#colorOff').click(function() {
+		$('#msg1').removeClass('red');
+	});
 
-    $('#swapColor').click(function() {
-        $('#msg2').toggleClass('red');
-    });
-});
+	$('#swapColor').click(function() {
+		$('#msg2').toggleClass('red');
+	});
+
+	$('#swapColor2').click(function() {
+		$('#msg3').addClass('red');
+		$('#msg3').removeClass('yellow');
+	});
+
+	$('#swapColor3').click(function() {
+		$('#msg3').addClass('yellow');
+		$('#msg3').removeClass('red');
+	});
+
+	$('#swapColor4').click(function() {
+		
+		if ($('#msg4').hasClass('red'||'blue')) {
+			$('#msg4').addClass('yellow');
+			$('#msg4').removeClass('red'||'blue')
+		} else if ($('#msg4').hasClass('yellow'||'red')) {
+			$('#msg4').addClass('blue');
+			$('#msg4').removeClass('yellow'||'red');
+		} else {
+			$('#msg4').addClass('red');
+			$('#msg4').removeClass('yellow'||'blue')
+		}})
+	});
+
--- WebContent/js/s51.js
+++ WebContent/js/s51.js
@@ -4,51 +4,89 @@
  * Slide 51
  */
 $(function() {
-    let important = false;
+	let important = false;
 
-    $('#changeMsg1').click(function() {
-        let msg = $('#msg1');
-        $('#target').html(msg.html());
-        if (important) {
-            msg.html('An <b>important</b> message');
-        } else {
-            msg.html('A <i>plain</i> message');
-        }
-        important = !important;
-    });
+	$('#changeMsg1').click(function() {
+		let msg = $('#msg1');
+		$('#target').html(msg.html());
+		if (important) {
+			msg.html('An <b>important</b> message');
+		} else {
+			msg.html('A <i>plain</i> message');
+		}
+		important = !important;
+	});
 
-    $('#changeTextMsg1').click(function() {
-        let msg = $('#msg1');
-        $('#target').text(msg.text());
-        if (important) {
-            msg.text('An <b>important</b> message');
-        } else {
-            msg.text('A <i>plain</i> message');
-        }
-        important = !important;
-    });
+	$('#changeTextMsg1').click(function() {
+		let msg = $('#msg1');
+		$('#target').text(msg.text());
+		if (important) {
+			msg.text('An <b>important</b> message');
+		} else {
+			msg.text('A <i>plain</i> message');
+		}
+		important = !important;
+	});
 
-    $('#changeMsg2').click(function() {
-        let msg = $('#msg2');
-        $('#target').text(msg.val());
-        if (important) {
-            msg.val('Important');
-        } else {
-            msg.val('Plain');
-        }
-        important = !important;
-    });
+	$('#changeMsg2').click(function() {
+		let msg = $('#msg2');
+		$('#target').text(msg.val());
+		if (important) {
+			msg.val('Important');
+		} else {
+			msg.val('Plain');
+		}
+		important = !important;
+	});
 
-    $('#upsize').click(function() {
-        let msg = $('#msg3');
-        let cur = parseInt(msg.css('font-size'));
-        msg.css('font-size', cur * 2);
-    });
+	$('#upsize').click(function() {
+		let msg = $('#msg3');
+		let cur = parseInt(msg.css('font-size'));
+		msg.css('font-size', cur * 2);
+	});
 
-    $('#downsize').click(function() {
-        let msg = $('#msg3');
-        let cur = parseInt(msg.css('font-size'));
-        msg.css('font-size', cur > 1 ? cur / 2 : 1);
-    });
+	$('#downsize').click(function() {
+		let msg = $('#msg3');
+		let cur = parseInt(msg.css('font-size'));
+		msg.css('font-size', cur > 1 ? cur / 2 : 1);
+	});
 
+	let wn = 0;
+	let wr = 0;
+	let tg = 0;
+
+	$("#roll").click(function() {
+		let n = Math.floor((Math.random() * 6) + 1);
+		tg = ++tg;
+		$('#msgex').val(n);
+		if (n > 3) {
+			$("<p>You won</p>").css({
+				color : 'green'
+			}).appendTo('#result');
+			// $('#resultarea').text($('#resultarea').text() + 'You won\n');
+			$('#resultarea').append('You won\n');
+			wn = ++wn;
+		} else if (n <= 3) {
+			$("<p>You lost</p>").css({
+				color : 'red'
+			}).appendTo('#result');
+			// $('#resultarea').text($('#resultarea').text() + 'You lost\n');
+			$('#resultarea').append('You lost\n');
+		}
+		wr = Math.floor((wn / tg) * 100);
+		$('#result').prop('title', 'Your win rate is ' + wr + '%\n');
+		if (n === 1) {
+			$('#dice').prop('src', './pic/dice1.png');
+		} else if (n === 2) {
+			$('#dice').prop('src', './pic/dice2.png');
+		} else if (n === 3) {
+			$('#dice').prop('src', './pic/dice3.png');
+		} else if (n === 4) {
+			$('#dice').prop('src', './pic/dice4.png');
+		} else if (n === 5) {
+			$('#dice').prop('src', './pic/dice5.png');
+		} else if (n === 6) {
+			$('#dice').prop('src', './pic/dice6.png');
+		}
+	})
 });
